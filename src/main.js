bf3e02720249ef7492cf939061b27d866b200bc3
Suhaiming Arsi <suhaiming41@gmail.com>
2021-06-29 09:17:19 +0700

    :wrench: import fortawesome

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
-// import './plugins/axios'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+import '@fortawesome/fontawesome-free/js/all'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  
   render: (h) => h(App),
 }).$mount('#app')
