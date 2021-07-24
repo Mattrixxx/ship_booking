abf6b56010d8592a0dce6d4933b9c8ccb642bde8
Suhaiming Arsi <suhaiming41@gmail.com>
2021-07-24 14:19:53 +0700

    :hammer: add components

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <b-button v-b-toggle.sidebar-no-header variant="success" id="side"
+      ><i class="fas fa-bars"></i
+    ></b-button>
+    <b-sidebar
+      id="sidebar-no-header"
+      aria-labelledby="sidebar-no-header-title"
+      no-header
+      shadow
+      backdrop
+      backdrop-variant="dark"
+    >
+      <template #footer="{ hide }">
+        <div class="d-flex text-light align-items-center px-3 py-2">
+          <strong class="mr-auto"></strong>
+          <b-button size="lg" @click="hide" variant="link"
+            ><i class="fas fa-angle-double-left"></i
+          ></b-button>
+        </div>
+      </template>
+      <div class="p-3">
+        <div class="d-flex align-items-center flex-column">
+          <!-- <h4 id="sidebar-no-header-title" class="mt-3 mb-3">Promtongyai</h4> -->
+          <div>
+            <b-img src="../img/3.png" height="100px"></b-img>
+          </div>
+        </div>
+        <nav class="mb-3">
+          <b-nav vertical>
+            <b-nav-item href="/users"
+              ><i class="fas fa-user"></i> User</b-nav-item
+            >
+            <b-nav-item href="/drivers"
+              ><i class="fas fa-user-tie"></i> Driver</b-nav-item
+            >
+            <b-nav-item href="/boats"
+              ><i class="fas fa-ship"></i> Boat</b-nav-item
+            >
+            <b-nav-item href="/rafts"
+              ><i class="fas fa-bed"></i> Raft</b-nav-item
+            >
+            <b-nav-item href="/packages"
+              ><i class="fas fa-suitcase-rolling"></i> Package</b-nav-item
+            >
+            <b-nav-item href="/bookings"
+              ><i class="fas fa-list-alt"></i> Booking</b-nav-item
+            >
+            <b-nav-item href="/sailings"
+              ><i class="fas fa-receipt"></i> Sailing</b-nav-item
+            >
+            <b-nav-item href="/reviews"
+              ><i class="fas fa-star"></i> Review</b-nav-item
+            >
+          </b-nav>
+        </nav>
+      </div>
+    </b-sidebar>
+
+    <router-view />
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+.nav-item > .nav-link {
+  /* background-color: pink; */
+  color: rgb(211, 169, 169);
+  font-size: 20px;
+  /* border: 1px solid rgb(226, 149, 162); */
+}
+.nav-item > .nav-link:hover {
+  background-color: rgb(196, 196, 196);
+  color: white;
+}
+.btn-link {
+  color: rgb(211, 169, 169);
+}
+.btn-link:hover {
+  color: rgb(199, 199, 199);
+}
+button#side {
+  width: 60px;
+  height: 60px;
+}
+.svg-inline--fa {
+  font-size: 20px;
+}
+.btn-success {
+  color: #fff;
+  border-color: #da8189 !important;
+  background: #da8189 !important;
+}
+</style>
---
+++ src/components/TabUser.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <sidebar/>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <template #button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+import sidebar from '../components/Sidebar.vue'
+
+export default {
+  components:{sidebar},
+}
+</script>
+
+<style scoped>
+.navbar {
+    padding:0px;
+}
+.navbar-collapse {
+    padding-right: 2.5%;
+}
+.bg-info {
+    background-color: #da8189!important;
+}
+</style>
